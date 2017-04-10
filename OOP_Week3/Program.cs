119cfb93082f841ff06d748cf0b733a9edb422f7
rlsizer <theryanlilo@gmail.com>
2017-04-10 13:09:04 -0400

    Edited main program.

--- OOP_Week3/Program.cs
+++ OOP_Week3/Program.cs
@@ -13,14 +13,9 @@ namespace OOP_Week3
 
             Car car1 = new Car("Phantom","Rolls-Royce","emerald green","2013",4,5,"RR21GT7V34753");
 
-            Console.WriteLine("My car is an " +car1.color+" "+ car1.year + " " + car1.make + " " + car1.model+".");
+            //Console.WriteLine("My car is an " +car1.color+" "+ car1.year + " " + car1.make + " " + car1.model+".");
 
-            car1.make = "Dodge";
-            car1.model = "Viper";
-            car1.color = "scarlet red";
-            car1.year = "2008";
-
-            Console.WriteLine("My car is a " + car1.color + " " + car1.year + " " + car1.make + " " + car1.model + ".");
+            
 
         }
     }
