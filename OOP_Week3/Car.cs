ff092cf96e9e3d7699809ac07e43fbfecbdcd7d5
rlsizer <theryanlilo@gmail.com>
2017-04-10 11:03:11 -0400

    Initial commit.

---
+++ OOP_Week3/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Week3
+{
+    class Car
+    {
+
+        private string model;
+        private string make;
+        private string color;
+        private int numOfDoors;
+        private int numOfSeats;
+
+
+    }
+}
